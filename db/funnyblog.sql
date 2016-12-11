c629797d149d48a1f28c204c51ddd7a83017a08d
eneko <enekosampedro@gmail.com>
2016-12-11 14:43:57 +0100

    Datu-basea moldatua

--- db/funnyblog.sql
+++ db/funnyblog.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.4.1deb2ubuntu2
+-- version 4.5.1
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: Dec 10, 2016 at 03:13 AM
--- Server version: 5.7.16-0ubuntu0.16.04.1
--- PHP Version: 7.0.8-0ubuntu0.16.04.3
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 11-12-2016 a las 14:43:15
+-- Versión del servidor: 10.1.16-MariaDB
+-- Versión de PHP: 7.0.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,13 +17,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `funnyblog`
+-- Base de datos: `funnyblog`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `berria`
+-- Estructura de tabla para la tabla `berria`
 --
 
 CREATE TABLE `berria` (
@@ -31,61 +31,23 @@ CREATE TABLE `berria` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `berria`
+-- Volcado de datos para la tabla `berria`
 --
 
 INSERT INTO `berria` (`ID`) VALUES
-(1);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `iruzkina`
---
-
-CREATE TABLE `iruzkina` (
-  `ID` int(11) NOT NULL,
-  `Iruzkina` varchar(1024) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `BerriaID` int(11) NOT NULL,
-  `Egilea` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `Data` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+(1),
+(2),
+(3);
 
 --
--- Indexes for dumped tables
+-- Índices para tablas volcadas
 --
 
 --
--- Indexes for table `berria`
+-- Indices de la tabla `berria`
 --
 ALTER TABLE `berria`
   ADD PRIMARY KEY (`ID`);
-
---
--- Indexes for table `iruzkina`
---
-ALTER TABLE `iruzkina`
-  ADD PRIMARY KEY (`ID`),
-  ADD KEY `BerriaID` (`BerriaID`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `iruzkina`
---
-ALTER TABLE `iruzkina`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `iruzkina`
---
-ALTER TABLE `iruzkina`
-  ADD CONSTRAINT `BerriarenID` FOREIGN KEY (`BerriaID`) REFERENCES `berria` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
